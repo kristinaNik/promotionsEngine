fd9a93f9de0d37e87c1df6106b7f1912a78905fa
Kristina Nikiforova <kristina.nik93@gmail.com>
2024-01-06 18:09:29 +0200

    Creating ProductsController

---
+++ src/Controller/ProductsController.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ProductsController extends AbstractController
+{
+
+}
