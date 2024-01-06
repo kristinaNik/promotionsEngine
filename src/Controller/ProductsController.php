88587f38f51c5eff6571babe2b81a8d33b7ea058
Kristina Nikiforova <kristina.nik93@gmail.com>
2024-01-06 20:12:28 +0200

    Added DTO and serialize and return the data

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -4,9 +4,44 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\DTO\LowestPriceEnquiryDTO;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class ProductsController extends AbstractController
 {
+    #[Route('products/{id}/lowest-price', name: 'lowest-price', methods: ['POST'])]
+    public function lowestPrice(Request $request, int $id, SerializerInterface $serializer): Response
+    {
+        //SerializerInterface is wired as an alias for the serializer
+
+        //1. Deserialize JSON data into DTO
+        /** @var LowestPriceEnquiryDTO $lowestPriceEnquiry */
+        $lowestPriceEnquiry = $serializer->deserialize(
+            $request->getContent(),
+            LowestPriceEnquiryDTO::class,
+            'json'
+        );
+
+
+        //2. Pass the Enquiry into promotion filter - the appropriate promotion will be applied
+        //3. Return the modified Enquiry
+        $lowestPriceEnquiry->setDiscountedPrice(50);
+        $lowestPriceEnquiry->setPrice(100);
+        $lowestPriceEnquiry->setPromotionId(3);
+        $lowestPriceEnquiry->setPromotionName('Black friday half price sale');
+
+        return new JsonResponse($lowestPriceEnquiry, 200);
+    }
+
+    #[Route('products/{id}/promotions', name: 'promotions', methods: 'GET')]
+    public function promotions(Request $request, int $id)
+    {
+
+    }
 
 }
---
+++ src/DTO/LowestPriceEnquiryDTO.php
@@ -0,0 +1,113 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\DTO;
+
+class LowestPriceEnquiryDTO implements \JsonSerializable
+{
+    private ?int $productId;
+    private ?int $quantity;
+    private ?string $requestLocation;
+    private ?string $voucherCode;
+    private ?string $requestDate;
+    private ?int $price;
+    private ?int $discountedPrice;
+    private ?int $promotionId;
+    private ?string $promotionName;
+
+    public function getProductId(): ?int
+    {
+        return $this->productId;
+    }
+
+    public function getQuantity(): ?int
+    {
+        return $this->quantity;
+    }
+
+    public function getRequestLocation(): ?string
+    {
+        return $this->requestLocation;
+    }
+
+    public function getVoucherCode(): ?string
+    {
+        return $this->voucherCode;
+    }
+
+    public function getRequestDate(): ?string
+    {
+        return $this->requestDate;
+    }
+
+    public function getPrice(): ?int
+    {
+        return $this->price;
+    }
+
+    public function getDiscountedPrice(): ?int
+    {
+        return $this->discountedPrice;
+    }
+
+    public function getPromotionId(): ?int
+    {
+        return $this->promotionId;
+    }
+
+    public function getPromotionName(): ?string
+    {
+        return $this->promotionName;
+    }
+
+    public function setProductId(?int $productId): void
+    {
+        $this->productId = $productId;
+    }
+
+    public function setQuantity(?int $quantity): void
+    {
+        $this->quantity = $quantity;
+    }
+
+    public function setRequestLocation(?string $requestLocation): void
+    {
+        $this->requestLocation = $requestLocation;
+    }
+
+    public function setVoucherCode(?string $voucherCode): void
+    {
+        $this->voucherCode = $voucherCode;
+    }
+
+    public function setRequestDate(?string $requestDate): void
+    {
+        $this->requestDate = $requestDate;
+    }
+
+    public function setPrice(?int $price): void
+    {
+        $this->price = $price;
+    }
+
+    public function setDiscountedPrice(?int $discountedPrice): void
+    {
+        $this->discountedPrice = $discountedPrice;
+    }
+
+    public function setPromotionId(?int $promotionId): void
+    {
+        $this->promotionId = $promotionId;
+    }
+
+    public function setPromotionName(?string $promotionName): void
+    {
+        $this->promotionName = $promotionName;
+    }
+
+    public function jsonSerialize(): mixed
+    {
+        return get_object_vars($this);
+    }
+}
